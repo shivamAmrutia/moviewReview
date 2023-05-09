437e043b5da31fc927f8790430f9caf91e8cf3f4
shivam <85829459+shivamAmrutia@users.noreply.github.com>
2023-05-09 10:34:19 +0530

    for CORS

--- src/main/java/dev/shivam/movies/MovieController.java
+++ src/main/java/dev/shivam/movies/MovieController.java
@@ -8,11 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/v1/movies")
 public class MovieController {
     @Autowired
--- src/main/java/dev/shivam/movies/ReviewController.java
+++ src/main/java/dev/shivam/movies/ReviewController.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
     @Autowired
